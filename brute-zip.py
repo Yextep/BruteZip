9f3ac5f8621c2ee909ebd1cf306eaed7b7722e12
Yextep <114537444+Yextep@users.noreply.github.com>
2023-11-07 09:29:23 -0500

    Update brute-zip.py

--- brute-zip.py
+++ brute-zip.py
@@ -1,20 +1,32 @@
 import pyzipper
+import sys
 
-def ataque_fuerza_bruta(archivo, diccionario):
+def ataque_fuerza_bruta(archivo, diccionario, length_based=False, longitud=None):
     with pyzipper.AESZipFile(archivo) as zip_ref:
-        for password in diccionario:
+        total_passwords = len(diccionario)
+        for index, password in enumerate(diccionario):
+            if length_based and len(password) != longitud:
+                continue
             try:
                 zip_ref.extractall(pwd=password.encode('utf-8'))
-                print(f'Contraseña encontrada: {password}')
+                print(f'\nContraseña encontrada: {password}')
                 return
             except Exception as e:
                 pass
+            # Calcula el progreso y muestra la barra de caracteres '*' en la misma línea
+            progress = (index + 1) / total_passwords
+            sys.stdout.write('\r[' + '*' * int(progress * 50) + ' ' * (50 - int(progress * 50)) + f'] {int(progress * 100)}%')
+            sys.stdout.flush()
 
-    print('Contraseña no encontrada')
+    print('\nContraseña no encontrada')
 
 def main():
     archivo = input('Introduce la ruta del archivo protegido: ')
     diccionario = input('Introduce la ruta del diccionario .txt: ')
+    longitud = None
+    length_based = input('¿Deseas realizar un ataque de fuerza bruta por longitud? (S/N): ')
+    if length_based.lower() == 's':
+        longitud = int(input('Introduce la longitud de contraseña a probar: '))
 
     try:
         with open(diccionario, 'r', encoding='utf-8', errors='replace') as f:
@@ -23,7 +35,7 @@ def main():
         print('El diccionario no existe')
         return
 
-    ataque_fuerza_bruta(archivo, diccionario)
+    ataque_fuerza_bruta(archivo, diccionario, length_based=True, longitud=longitud)
 
 if __name__ == '__main__':
     main()
